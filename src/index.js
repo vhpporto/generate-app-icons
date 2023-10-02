e9611c0e7697b4389b0904a7660b0a461cf9af94
vhpporto <vhpporto@gmail.com>
2023-10-02 10:27:22 -0300

    feat: generate ios icons

--- src/index.js
+++ src/index.js
@@ -3,8 +3,38 @@
 const sharp = require("sharp");
 const fs = require("fs");
 const chalk = require("chalk");
-
+const yargs = require("yargs/yargs");
 const path = require("path");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
+
+const iosSizes = [
+  { name: "ipad-20x20-1x.png", size: 20 },
+  { name: "ipad-20x20-2x.png", size: 40 },
+  { name: "ipad-29x29-1x.png", size: 29 },
+  { name: "ipad-29x29-2x.png", size: 58 },
+  { name: "ipad-40x40-1x.png", size: 40 },
+  { name: "ipad-40x40-2x.png", size: 80 },
+  { name: "ipad-50x50-1x.png", size: 50 },
+  { name: "ipad-50x50-2x.png", size: 100 },
+  { name: "ipad-72x72-1x.png", size: 72 },
+  { name: "ipad-72x72-2x.png", size: 144 },
+  { name: "ipad-76x76-1x.png", size: 76 },
+  { name: "ipad-76x76-2x.png", size: 152 },
+  { name: "ipad-83.5x83.5-2x.png", size: 167 },
+  { name: "iphone-20x20-2x.png", size: 40 },
+  { name: "iphone-20x20-3x.png", size: 60 },
+  { name: "iphone-29x29-1x.png", size: 29 },
+  { name: "iphone-29x29-2x.png", size: 58 },
+  { name: "iphone-29x29-3x.png", size: 87 },
+  { name: "iphone-40x40-2x.png", size: 80 },
+  { name: "iphone-40x40-3x.png", size: 120 },
+  { name: "iphone-57x57-1x.png", size: 57 },
+  { name: "iphone-57x57-2x.png", size: 114 },
+  { name: "iphone-60x60-2x.png", size: 120 },
+  { name: "iphone-60x60-3x.png", size: 180 },
+  { name: "ios-marketing-1024x1024-1x.png", size: 1024 },
+];
 
 const SIZES = [
   {
@@ -41,6 +71,13 @@ const SIZES = [
 
 const OUTPUT_BASE_DIRECTORY = "output/icons";
 const BORDER_RADIUS = 6;
+
+const sourceJsonPath = path.join(__dirname, "Contents.json");
+const destinationJsonPath = path.join(
+  __dirname,
+  `../${OUTPUT_BASE_DIRECTORY}/ios/`,
+  "Contents.json"
+);
 
 const ensureDirectoryExists = (dirPath) => {
   if (!fs.existsSync(dirPath)) {
@@ -105,7 +142,25 @@ const createBorderedIcon = async (inputImagePath, outputPath, config) => {
     .toFile(outputPath);
 };
 
-const generateIcons = async (config, inputImagePath) => {
+const generateIosIcons = async (config, inputImagePath) => {
+  const dpiDirectory = path.join(OUTPUT_BASE_DIRECTORY, "ios");
+  ensureDirectoryExists(dpiDirectory);
+  try {
+    const outputPath = path.join(dpiDirectory, config.name);
+    await sharp(inputImagePath)
+      .resize(config.size, config.size)
+      .toFile(outputPath);
+    console.info(
+      chalk.green(`✓ iOS icon ${config.name} generated in ${dpiDirectory}!`)
+    );
+  } catch (err) {
+    console.error(
+      chalk.red(`Error generating iOS icon ${config.name}: ${err.message}`)
+    );
+  }
+};
+
+const generateAndroidIcons = async (config, inputImagePath) => {
   const dpiDirectory = path.join(OUTPUT_BASE_DIRECTORY, config.name);
   try {
     ensureDirectoryExists(dpiDirectory);
@@ -130,7 +185,7 @@ const generateIcons = async (config, inputImagePath) => {
 
     await Promise.all(tasks);
     console.info(
-      chalk.green(`Icons for ${config.name} generated in ${dpiDirectory}!`)
+      chalk.green(`✓ Icons for ${config.name} generated in ${dpiDirectory}!`)
     );
   } catch (err) {
     console.error(
@@ -141,10 +196,31 @@ const generateIcons = async (config, inputImagePath) => {
 
 if (require.main === module) {
   const inputImagePathFromCLI = process.argv[2];
-  const generationTasks = SIZES.map((config) =>
-    generateIcons(config, inputImagePathFromCLI)
-  );
-  Promise.all(generationTasks);
+  let generationTasks = [];
+
+  if (argv.platform.includes("android")) {
+    generationTasks = generationTasks.concat(
+      SIZES.map((config) => generateAndroidIcons(config, inputImagePathFromCLI))
+    );
+  }
+
+  if (argv.platform.includes("ios")) {
+    generationTasks = generationTasks.concat(
+      iosSizes.map((config) => generateIosIcons(config, inputImagePathFromCLI))
+    );
+  }
+
+  Promise.all(generationTasks).catch((error) => {
+    console.error(chalk.red(`Failed to generate some icons: ${error.message}`));
+  });
+
+  fs.copyFile(sourceJsonPath, destinationJsonPath, (err) => {
+    if (err) throw err;
+    console.info(chalk.green("✓ Contents.json successfully created"));
+  });
 }
 
-module.exports = generateIcons;
+module.exports = {
+  generateAndroidIcons,
+  generateIosIcons,
+};
